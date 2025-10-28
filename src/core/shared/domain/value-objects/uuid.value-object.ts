ae72594f515671c8703828333b9bb0042e69a662
Gabriel Dezena Torres <gabriel.deztor@gmail.com>
2025-10-28 09:48:44 -0300

    feat: add UUID value object for domain entities

---
+++ src/core/shared/domain/value-objects/uuid.value-object.ts
@@ -0,0 +1,27 @@
+import crypto from 'crypto';
+import { validate as uuidValidate } from 'uuid';
+import { ValueObject } from './value-object';
+
+export class Uuid extends ValueObject<string> {
+  constructor(id?: string) {
+    super(id || crypto.randomUUID());
+    this.validate();
+  }
+
+  private validate() {
+    const isValid = uuidValidate(this.value);
+    if (!isValid) {
+      throw new InvalidUuidError(this.value);
+    }
+    return this.value;
+  }
+}
+
+export class InvalidUuidError extends Error {
+  constructor(invalidValue: string) {
+    super(`Value ${invalidValue} must be a valid UUID`);
+    this.name = 'InvalidUuidError';
+  }
+}
+
+export default Uuid;
