c04a789094b5445fb6f2d57abb694c56b9f4c185
Gabriel Dezena Torres <gabriel.deztor@gmail.com>
2025-10-28 09:32:43 -0300

    refactor: enhance Name value object validation

--- src/core/shared/domain/value-objects/name.value-object.spec.ts
+++ src/core/shared/domain/value-objects/name.value-object.spec.ts
@@ -1,15 +1,6 @@
-import { Customer } from 'src/core/events/domain/entities/custumer.entity';
 import { Name } from './name.value-object';
 
 test('should create a name value object', () => {
   const name = new Name('John Doe');
   expect(name.value).toBe('John Doe');
-
-  const custumer = new Customer({
-    name,
-    cpf: '12345678901',
-  });
-
-  custumer.name = new Name('Jane Doe');
-  expect(custumer.name.value).toBe('Jane Doe');
 });
--- src/core/shared/domain/value-objects/name.value-object.ts
+++ src/core/shared/domain/value-objects/name.value-object.ts
@@ -1,3 +1,12 @@
 import { ValueObject } from './value-object';
 
-export class Name extends ValueObject<string> {}
+export class Name extends ValueObject<string> {
+  constructor(value: string) {
+    super(value);
+    this.isValid();
+  }
+
+  public isValid() {
+    return this.value.length > 3;
+  }
+}
